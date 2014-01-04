f5c260c5e926f4f93f7b8a04ee9c065baa5faf5e
Kyle Kellogg <kyle@kylekellogg.com>
2014-01-04 03:02:46 -0500

    Updated main.lua to use beetle debugging dependency and new player class.

--- main.lua
+++ main.lua
@@ -1,16 +1,15 @@
-
+require "libs/Beetle"
+require "player"
 
 function love.load()
-	player = {
-		x = 25,
-		y = 25,
-		width = 50,
-		height = 50,
-		vel = {
-			x = 0,
-			y = 0
-		}
-	}
+	beetle.load()
+	beetle.setKey( "`" )
+
+	player = Player:new()
+
+	beetle.add( "Player X", player.x )
+	beetle.add( "Player Y", player.y )
+	beetle.add( "Player Velocity", player.vel )
 
 	input = {
 		up = false,
@@ -18,28 +17,77 @@ function love.load()
 		left = false,
 		right = false
 	}
+
+	love.graphics.setBackgroundColor( 255, 255, 255 )
+end
+
+function love.update( dt )
+	if input.left then
+		player.vel.x = player.vel.x - player.speed.x
+	end
+	if input.right then
+		player.vel.x = player.vel.x + player.speed.x
+	end
+
+	if input.up then
+		if player.jumpCounter < player.maxJumpCounter then
+			player.vel.y = math.max( player.vel.y + ((player.maxJumpCounter - player.jumpCounter) * -(player.speed.y * math.pi)), -player.maxVel.y )
+			player.jumpCounter = player.jumpCounter + 1
+		else
+			player.jumpCounter = 0
+			input.up = false
+		end
+	end
+	if input.down then
+		player.vel.y = player.vel.y + player.speed.y
+	end
+
+	if not input.up and player.y < love.graphics.getHeight() - (player.height * 0.5) then
+		player.vel.x = math.max( math.min( player.vel.x * 0.94, player.maxVel.x ), -player.maxVel.x )
+	else
+		player.vel.x = math.max( math.min( player.vel.x * 0.92, player.maxVel.x ), -player.maxVel.x )
+	end
+	player.vel.y = math.max( math.min( player.vel.y + 0.81, player.maxVel.y ), -player.maxVel.y )
+
+	player.x = player.x + player.vel.x
+	player.y = player.y + player.vel.y
+
+	if player.x > love.graphics.getWidth() - (player.width * 0.5) then
+		player.x = love.graphics.getWidth() - (player.width * 0.5)
+		player.vel.x = 0
+	elseif player.x < player.width * 0.5 then
+		player.x = player.width * 0.5
+		player.vel.x = 0
+	end
+
+	if player.y > love.graphics.getHeight() - (player.height * 0.5) then
+		player.y = love.graphics.getHeight() - (player.height * 0.5)
+		player.doubleJump = false
+	elseif player.y < player.height * 0.5 then
+		player.y = player.height * 0.5
+		player.vel.y = 0
+	end
+
+	beetle.update( "Player X", player.x )
+	beetle.update( "Player Y", player.y )
+	beetle.update( "Player Velocity", player.vel )
 end
 
 function love.draw()
-	if input.up then
-		player.vel.y = math.max( player.y - player.height, player.height * 0.5 )
-	end
-	if input.down then
-		player.y = math.min( player.y + player.height, love.graphics.getHeight() - (player.height * 0.5) )
-	end
-	if input.left then
-		player.x = math.max( player.x - player.width, player.width * 0.5 )
-	end
-	if input.right then
-		player.x = math.min( player.x + player.width, love.graphics.getWidth() - (player.width * 0.5) )
-	end
+	love.graphics.setColor( 0, 0, 0 )
+	love.graphics.rectangle( "fill", player.x - (player.width * 0.5), player.y - (player.height * 0.5), player.width, player.height )
 
-	love.graphics.rectangle( 'fill', player.x - (player.width * 0.5), player.y - (player.height * 0.5), player.width, player.height )
+	beetle.draw()
 end
 
 function love.keypressed( key )
 	if key == "up" then
-		input.up = true
+		if player.y >= (love.graphics.getHeight() - (player.height * 0.5)) then
+			input.up = true
+		elseif not player.doubleJump then
+			player.doubleJump = true
+			input.up = true
+		end
 	end
 	if key == "down" then
 		input.down = true
@@ -65,4 +113,10 @@ function love.keyreleased( key )
 	if key == "right" then
 		input.right = false
 	end
+
+	beetle.key( key )
+end
+
+function map( val, omin, omax, nmin, nmax )
+	return nmin + (nmax - nmin) * ( (val - omin) / (omax - omin) )
 end
