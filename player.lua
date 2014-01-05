e54f7ea0abd33851587000b49dae80bef3f2f42e
Kyle Kellogg <kyle@kylekellogg.com>
2014-01-05 08:53:13 -0500

    Player now extends DisplayObject, has some accessor methods for x and y

--- player.lua
+++ player.lua
@@ -1,33 +1,28 @@
-local class = require "30log"
-local vector = require "libs/hump/vector"
-local Timer = require "libs/hump/timer"
-
-Player = class()
+Player = DisplayObject:extends()
 Player.__name = "Player"
 
 function Player:__init( _x, _y, _width, _height, _vel, _mxvel, _spd )
+	Player.super.__init( self, _x, _y, _width, _height, "dynamic", 1 )
+
 	self.vel = _vel and vector( _vel.x or 0, _vel.y or 0 ) or vector( 0, 0 )
 	self.maxVel = _mxvel and vector( _mxvel.x or 10, _mxvel.y or 10 ) or vector( 10, 10 )
+	self.omaxVel = self.maxVel:clone()
 	self.speed = _spd and vector( _spd.x or 1, _spd.y or 1 ) or vector( 1, 1 )
 	self.ospeed = self.speed:clone()
 
-	self.x = _x or love.graphics.getWidth() * 0.5
-	self.y = _y or love.graphics.getHeight() * 0.5
-
-	self.width = _width or 50
-	self.height = _height or 50
-
-	self.jumpCounter = 0
-	self.maxJumpCounter = 10
+	self.jumping = false
 	self.doubleJump = false
 	self.canDoubleJump = false
+
+	self.wasHittingWall = false
+	self.wasHittingGround = false
+	self.powerGroundHit = false
 
 	self.resetCanDoubleJumpTimerHandler = nil
 end
 
 local function __resetCanDoubleJump( instance )
 	if instance and instance.resetCanDoubleJumpTimerHandler then
-		print( 'reset!' )
 		instance.canDoubleJump = false
 		instance.resetCanDoubleJumpTimerHandler = nil
 	end
@@ -35,7 +30,6 @@ end
 
 function Player:resetCanDoubleJump()
 	if not self.canDoubleJump and not self.resetCanDoubleJumpTimerHandler then
-		print( 'waiting to reset...' )
 		self.canDoubleJump = true
 		self.resetCanDoubleJumpTimerHandler = Timer.add( 0.5, function() __resetCanDoubleJump( self ) end )
 	end
